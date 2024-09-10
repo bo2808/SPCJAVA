fffb1333bf452e7ef023162f85b39cf40f57022a
bo2808 <tuannguyenhoang389@gmail.com>
2024-09-10 08:36:28 +0700

    hello thế giới

---
+++ src/spc/edu/bai1hellword.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+
+/**
+ *
+ * @author Administrator
+ */
+public class bai1hellword {
+    public static void main(String[] args) {
+        System.out.println("hello wrold!");
+    }
+}
